7ddbc06e5767b9394a95aa53757265b8c9c9de20
Stephen Brown II <stephen.brownii@rackspace.com>
2018-09-26 11:20:21 -0500

    Add screensaver role

---
+++ roles/screensaver/files/lock.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+hash light 2> /dev/null
+if [ $? -eq 0 ]; then
+    BRIGHTNESS=true
+fi
+
+if ! pidof physlock > /dev/null; then
+    if [ "$BRIGHTNESS" = true ]; then
+        light -O
+        light -S 10
+    fi
+    /usr/bin/i3lock -fec 000000
+    /usr/sbin/physlock -md
+    if [ "$BRIGHTNESS" = true ]; then
+        light -I
+    fi
+fi
